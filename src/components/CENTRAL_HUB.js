521c7629a47fb90d268bbe8b92a16e6bc3c31e77
Walter González-Plata Cortes <Walter@192.168.1.19>
2021-07-24 19:54:55 -0500

    Setting API url to a constant

--- src/components/CENTRAL_HUB.js
+++ src/components/CENTRAL_HUB.js
@@ -5,11 +5,13 @@ import Coin from './Coin.js'
 
 function CENTRAL_HUB() {
 
+const API = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
 const [coins, setCoins] = useState([])
+
 
 useEffect(() => {
     axios
-    .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .get(API)
     .then(res => {
         console.log(res)
         setCoins(res.data)
