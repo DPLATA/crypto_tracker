ae48ad0fcb62f149833a5bf12b610137872769a7
Walter González-Plata Cortes <Walter@192.168.1.19>
2021-07-24 20:30:26 -0500

    Coin component for individual currenct with specs and images

--- src/components/COIN.js
+++ src/components/COIN.js
@@ -1,1 +1,23 @@
-TODO: Create functional component that renders a single ccrypto
+import React from 'react'
+import '../css/COIN_STYLES.css'
+
+function COIN(props) {
+
+    const { name, symbol, img, price, high, low, marketcap} = props
+
+    return (
+    <div>
+        <div className= 'Coin-item'>
+            <div className='Coin-spec'>{name}</div>
+            <div className='Coin-spec'>{symbol}</div>
+            <div className='Coin-spec'><img className='Coin-image' src={img} /> </div>
+            <div className='Coin-spec'>{price}</div>
+            <div className='Coin-spec'>{high}</div>
+            <div className='Coin-spec'>{low}</div>
+            <div className='Coin-spec'>{marketcap}</div>
+          </div>
+    </div>
+    )
+}
+
+export default COIN
