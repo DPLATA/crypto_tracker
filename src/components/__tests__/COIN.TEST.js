25d89c3d36a3edd56e2d8a78d57c3aa51acdc0f1
DPLATA <diegogplata@gmail.com>
2021-07-27 19:56:41 -0500

    💡update todos in coin unit test✨✅update coin unit test

--- src/components/__tests__/COIN.TEST.js
+++ src/components/__tests__/COIN.TEST.js
@@ -7,23 +7,25 @@ afterEach(() => {
 })
 
 test('Should render COIN component',async () => {
+    const TESTABLE_COINS = ['bitcoin', 'ethereum', 'dogecoin', 'cardano', 'chainlink', 'polkadot', 'litecoin', 'dai', 'monero', 'neo']
+
     const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
-    const coins = await axios.get(API_URL).then(response => {
+    const COINS = await axios.get(API_URL).then(response => {
         return response.data
     })
 
+    let random_coin = ''
+
     //Render all coins from API
-    render(coins.map(coin => 
+    render(COINS.map(coin => 
         <COIN key={coin.id} id={coin.id} name={coin.name} img={coin.image} 
         symbol={coin.symbol} price={coin.current_price} 
         high={coin.high_24h} low={coin.low_24h} marketcap={coin.market_cap}/>
     ))
 
-    //TODO: update this to check for 5 random coins out of an array of 10
-    const bitcoin = screen.getByTestId('bitcoin')
-    const ethereum = screen.getByTestId('ethereum')
-    const dogecoin = screen.getByTestId('dogecoin')
-    expect(bitcoin).toBeInTheDocument();
-    expect(ethereum).toBeInTheDocument();
-    expect(dogecoin).toBeInTheDocument();
+    for (let index = 0; index < TESTABLE_COINS.length / 2; index++) {
+        random_coin = TESTABLE_COINS[Math.floor(Math.random() * (6-0) + 0)]
+        
+        expect(screen.getByTestId(random_coin)).toBeInTheDocument()
+    }
 })
