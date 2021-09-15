960d59ec15ba2086049134dd49041250cda864ab
Diego Plata <diegogplata@gmail.com>
2021-09-15 17:49:42 -0500

    double dip on testing prettier setup with pre-commit hook🎨

--- src/components/CENTRAL_HUB.js
+++ src/components/CENTRAL_HUB.js
@@ -1,37 +1,43 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import COIN from './COIN.js'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import COIN from './COIN.js';
 
 function CENTRAL_HUB() {
+	const API_URL =
+		'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+	const [coins, setCoins] = useState([]);
 
-const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
-const [coins, setCoins] = useState([])
+	useEffect(() => {
+		axios
+			.get(API_URL)
+			.then((res) => {
+				//TODO: remove console logs not necessarily right away but should not be there
+				console.log(res);
+				setCoins(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
-
-useEffect(() => {
-    axios
-    .get(API_URL)
-    .then(res => {
-        //TODO: remove console logs not necessarily right away but should not be there
-        console.log(res)
-        setCoins(res.data)
-    },)
-    .catch(err => {
-        console.log(err)
-    })
-}, [])
-
-
-    return (
-        <div>
-            {coins.map(coin => 
-                <COIN key={coin.id} id={coin.id} name={coin.name} img={coin.image} 
-                symbol={coin.symbol} price={coin.current_price} 
-                high={coin.high_24h} low={coin.low_24h} marketcap={coin.market_cap}/>
-            )}
-        </div>
-    )
+	return (
+		<div>
+			{coins.map((coin) => (
+				<COIN
+					key={coin.id}
+					id={coin.id}
+					name={coin.name}
+					img={coin.image}
+					symbol={coin.symbol}
+					price={coin.current_price}
+					high={coin.high_24h}
+					low={coin.low_24h}
+					marketcap={coin.market_cap}
+				/>
+			))}
+		</div>
+	);
 }
 
-export default CENTRAL_HUB
+export default CENTRAL_HUB;
--- src/components/COIN.js
+++ src/components/COIN.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import '../css/COIN_STYLES.css'
+import React from 'react';
+import '../css/COIN_STYLES.css';
 
 function COIN(props) {
+	const { name, symbol, img, price, high, low, marketcap, id } = props;
 
-    const { name, symbol, img, price, high, low, marketcap, id } = props
-
-    return (
-    //TODO: from now on remember to set data-testid attributes to html elements for testing purposes
-        <div className= 'Coin-item' data-testid={id}>
-            <div className='Coin-spec'>{name}</div>
-            <div className='Coin-spec'>{symbol}</div>
-            {/* TODO: add an alt attribute to img tag */}
-            <div className='Coin-spec'><img className='Coin-image' src={img}/> </div>
-            <div className='Coin-spec'>{price}</div>
-            <div className='Coin-spec'>{high}</div>
-            <div className='Coin-spec'>{low}</div>
-            <div className='Coin-spec'>{marketcap}</div>
-        </div>
-    )
+	return (
+		//TODO: from now on remember to set data-testid attributes to html elements for testing purposes
+		<div className='Coin-item' data-testid={id}>
+			<div className='Coin-spec'>{name}</div>
+			<div className='Coin-spec'>{symbol}</div>
+			{/* TODO: add an alt attribute to img tag */}
+			<div className='Coin-spec'>
+				<img className='Coin-image' src={img} />{' '}
+			</div>
+			<div className='Coin-spec'>{price}</div>
+			<div className='Coin-spec'>{high}</div>
+			<div className='Coin-spec'>{low}</div>
+			<div className='Coin-spec'>{marketcap}</div>
+		</div>
+	);
 }
 
-export default COIN
+export default COIN;
